33ea17d54850d259fafd77b98c3ee7728e2e569a
simin.chen <915734467@qq.com>
2020-01-07 16:13:34 +0800

    我的spring-sample工程

--- spring-sample/src/main/java/org/springframework/mybatis/MybatisApplication.java
+++ spring-sample/src/main/java/org/springframework/mybatis/MybatisApplication.java
@@ -1,14 +1,9 @@
 package org.springframework.mybatis;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.mybatis.config.RunApplicationConfig;
-
 public class MybatisApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RunApplicationConfig.class);
-		System.out.println(applicationContext.getApplicationName());
+
 	}
 
 }
